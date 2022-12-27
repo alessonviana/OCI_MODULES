cab42efa9be0112ed49fbb9ad0be3f35c959fd52
gft102 <gft102@localiza.com>
2022-12-27 11:17:13 -0300

    remove outputs

--- instance/outputs.tf
+++ instance/outputs.tf
@@ -1,8 +1,8 @@
-output "ssh-with-k3s-user" {
-  value = format(
-    "\nssh -o StrictHostKeyChecking=no -i %s -l %s %s\n",
-    local_file.ssh_private_key.filename,
-    "k3s",
-    oci_core_instance._[1].public_ip
-  )
-}
+#output "ssh-with-k3s-user" {
+#  value = format(
+#    "\nssh -o StrictHostKeyChecking=no -i %s -l %s %s\n",
+#    local_file.ssh_private_key.filename,
+#    "k3s",
+#    oci_core_instance._[1].public_ip
+#  )
+#}
