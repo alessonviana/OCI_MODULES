55d4f21b98481d6e110c0916f7c1e5bdc7738ae0
gft102 <gft102@localiza.com>
2022-12-27 11:52:39 -0300

    up project

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,13 @@
-/*
-output "ssh-with-k3s-user" {
-  value = format(
-    "\nssh -o StrictHostKeyChecking=no -i %s -l %s %s\n",
-    local_file.ssh_private_key.filename,
-    "k3s",
-    oci_core_instance._[1].public_ip
-  )
-}
-
 output "compartment_id" {
   description = "Compartment ocid"
   // This allows the compartment ID to be retrieved from the resource if it exists, and if not to use the data source.
-  value = var.compartment_create ? element(concat(oci_identity_compartment.this.*.id, list("")), 0) : lookup(local.compartment_ids[0], "id")
+  value = var.compartment_create ? element(concat(oci_identity_compartment.this.*.id, [""]), 0) : lookup(local.compartment_ids[0], "id")
 }
 
 output "parent_compartment_id" {
   description = "Parent Compartment ocid"
   // This allows the compartment ID to be retrieved from the resource if it exists, and if not to use the data source.
-  value = var.compartment_create ? element(concat(oci_identity_compartment.this.*.compartment_id, list("")), 0) : lookup(local.parent_compartment_ids[0], "compartment_id")
+  value = var.compartment_create ? element(concat(oci_identity_compartment.this.*.compartment_id, [""]), 0) : lookup(local.parent_compartment_ids[0], "compartment_id")
 }
 
 output "compartment_name" {
@@ -30,18 +20,13 @@ output "compartment_description" {
   value = var.compartment_description
 }
 
-output "ssh-with-ubuntu-user" {
-  value = join(
-    "\n",
-    [for i in oci_core_instance._ :
-      format(
-        "ssh -o StrictHostKeyChecking=no -l ubuntu -p 22 -i %s %s # %s",
-        local_file.ssh_private_key.filename,
-        i.public_ip,
-        i.display_name
-      )
-    ]
+output "ssh-with-k3s-user" {
+  value = format(
+    "\nssh -o StrictHostKeyChecking=no -i %s -l %s %s\n",
+    local_file.ssh_private_key.filename,
+    "k3s",
+    oci_core_instance._[1].public_ip
   )
 }
 
-*/
+
--- variables.tf
+++ variables.tf
@@ -1,3 +1,77 @@
+#####################
+# compartment
+#####################
+
+variable "tenancy_ocid" {
+  type = string
+  description = "(Deprecated) The OCID of the tenancy."
+  default = null
+}
+
+variable "compartment_id" {
+  type = string
+  description = "The OCID of the parent compartment containing the compartment. Allow for sub-compartments creation"
+  default     = null
+}
+
+variable "compartment_name" {
+  type = string
+  description = "The name you assign to the compartment during creation. The name must be unique across all compartments in the tenancy. "
+  default     = null
+}
+
+// The description is only used if compartment_create = true.
+variable "compartment_description" {
+  type = string
+  description = "The description you assign to the compartment. Does not have to be unique, and it's changeable. "
+  default     = null
+}
+
+variable "compartment_create" {
+  type = bool
+  description = "(Deprecated) Create the compartment or not. If true, the compartment will be managed by this module, and the user must have permissions to create the compartment; If false, compartment data will be returned about the compartment if it exists, if not found, then an empty string will be returned for the compartment ID."
+  default     = true
+}
+
+variable "enable_delete" {
+  type = bool
+  description = "Enable compartment delete on destroy. If true, compartment will be deleted when `terraform destroy` is executed; If false, compartment will not be deleted on `terraform destroy` execution"
+  default     = true
+}
+
+##################
+# instance
+##################
+
+variable "shape" {
+  type    = string
+  default = "VM.Standard.A1.Flex"
+}
+
+variable "operating_system" {
+  type  = string
+  default  = "Canonical Ubuntu"
+}
+
+variable "operating_system_version" {
+  type  = string
+  default  = "20.04"
+}
+
+variable "memory_in_gbs_per_node" {
+  type    = number
+  default = 4
+}
+ variable "ocpus_per_node" {
+  type    = number
+  default = 1
+}
+
+ variable "how_many_nodes" {
+  type    = number
+  default = 3
+}
+
 /*
 Available flex shapes:
 "VM.Optimized3.Flex"  # Intel Ice Lake
