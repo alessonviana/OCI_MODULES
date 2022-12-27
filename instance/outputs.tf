9a7114d1fe8168b8a37756bcab43ab83e9c6f987
gft102 <gft102@localiza.com>
2022-12-27 09:44:25 -0300

    remove compartment ID outputs

--- instance/outputs.tf
+++ instance/outputs.tf
@@ -6,9 +6,3 @@ output "ssh-with-k3s-user" {
     oci_core_instance._[1].public_ip
   )
 }
-
-output "compartment_id" {
-  description = "Compartment ocid"
-  // This allows the compartment ID to be retrieved from the resource if it exists, and if not to use the data source.
-  value = var.compartment_create ? element(concat(oci_identity_compartment.this.*.id, list("")), 0) : lookup(local.compartment_ids[0], "id")
-}
